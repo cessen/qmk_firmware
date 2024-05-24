c3468c1bf2b1a5622f09456594bd955abf4185ad
Nathan Vegdahl <cessen@cessen.com>
2024-05-24 08:10:48 +0200

    Flesh out the key map to more-or-less match my glove80.

--- keyboards/splitkb/kyria/keymaps/cessen/rules.mk
+++ keyboards/splitkb/kyria/keymaps/cessen/rules.mk
@@ -1,3 +1,5 @@
+TAP_DANCE_ENABLE = yes
+
 OLED_ENABLE = no
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no    # Disable keyboard RGB matrix, as it is enabled by default on rev3
